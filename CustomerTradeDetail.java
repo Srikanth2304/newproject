093da3743033a34afdca7fbdc19ef1897bd1bd70
Srikanth2304 <59932476+Srikanth2304@users.noreply.github.com>
2023-08-28 11:39:04 +0530

    Create CustomerTradeDetail.java

---
+++ CustomerTradeDetail.java
@@ -0,0 +1,15 @@
+package com.scb.tradeapi.response;
+
+public class CustomTradeDetail {
+	private long id;
+	private String product;
+	private String party1;
+	private String party2;
+	private String ExchangedCurrency1;
+	private String ExchangedCurrency2;
+	private double Amount;
+	private String Date;
+	private String Status;
+	private String MakersAction;
+	private String CheckersAction;
+}
